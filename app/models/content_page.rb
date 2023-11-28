d4687942ade2eb95aca73fcc667cd9c8e05f890a
h8rry <harrychenca@gmail.com>
2023-11-28 11:40:23 +0800

    Fixes

--- app/models/content_page.rb
+++
@@ -1,5 +0,0 @@
-class ContentPage < ApplicationRecord
-   validates :content, presence: true
-
-   belongs_to :username, required: false
-end
--- db/migrate/20231127004248_create_content_pages.rb
+++
@@ -1,9 +0,0 @@
-class CreateContentPages < ActiveRecord::Migration[6.1]
-  def change
-    create_table :content_pages do |t|
-
-      t.string :content
-      t.timestamps
-    end
-  end
-end
